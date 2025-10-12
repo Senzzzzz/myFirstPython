97ff61f89758f8cad2f6e8ee0367eff609362b90
Senzzzzz <senzhao123@gmail.com>
2025-10-12 21:01:45 +0800

    practice pyqt6

--- practice.py
+++ practice.py
@@ -1,27 +1,30 @@
-def online_store():
-    apple = 200
-    banana = 400
-    orange = 600
-    combo1 = apple + banana - (0.10 * (apple + banana))
-    combo2 = banana + orange - (0.10 * (banana + orange))
-    combo3 = apple + orange - (0.10 * (apple + orange))
-    Gift_Pack = apple + banana + orange - (0.25 * (apple + banana + orange))
-
-    print("Output: ")
-    print("Online Store")
-    print("----------------------")
-    print("Product(S)                           Price")
-    print(f"Apple                                {apple:.2f}")
-    print(f"Banana                               {banana:.2f}")
-    print(f"Orange                               {orange:.2f}")
-    print(f"Combo 1 (apple + banana)             {combo1:.2f}")
-    print(f"Combo 2 (banana + orange)            {combo2:.2f}")
-    print(f"Combo 3 (apple + orange)             {combo3:.2f}")
-    print(f"Gift Pack (apple + banana + orange)  {Gift_Pack:.2f}")
-    print("_________________________________")
-    print("For delivery Contact: 1234567890")
+import datetime
+import time
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QCheckBox, QRadioButton, QButtonGroup, QLineEdit, QPushButton
+from PyQt5.QtGui import QIcon, QFont, QPixmap, QFontDatabase
+from PyQt5.QtCore import Qt, QDateTime, QTimer, QTime, QElapsedTimer
+import pytesseract
 
 
-online_store()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 300, 400, 500)
+        self.setWindowIcon(QIcon("pyqt5icons/sunny.jpg"))
+        label = QLabel("asdasd", self)
 
-# The function online_store() defines individual product prices, then calculates discounted prices for combos. Discounts are computed using arithmetic operations, applying either 10% or 25% off depending on the combo. The function uses formatted printing (f-strings) to show the product catalog neatly, demonstrating basic procedural programming with function structure and formatted output. No external sources were used to create this function.
+    def initUi():
+        pass
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import time
-
-
-while True:
-    date = datetime.datetime.now()
-    time.sleep(1)
-    print(date.strftime("%H:%M:%S"))
