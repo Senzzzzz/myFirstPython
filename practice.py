68434fcd31e0c3186d880c3b2a8a4e4b0efc0c46
Senzzzzz <senzhao123@gmail.com>
2025-10-13 18:50:02 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -2,33 +2,43 @@ import datetime
 import time
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QCheckBox, QRadioButton, QButtonGroup, QLineEdit, QPushButton
-from PyQt5.QtGui import QIcon, QFont, QPixmap, QFontDatabase
-from PyQt5.QtCore import Qt, QDateTime, QTimer, QTime, QElapsedTimer
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QCheckBox, QRadioButton, QButtonGroup, QLineEdit, QPushButton
+from PyQt6.QtGui import QIcon, QFont, QPixmap, QFontDatabase
+from PyQt6.QtCore import Qt, QDateTime, QTimer, QTime, QElapsedTimer
 import pytesseract
 
 
-class MainWindow(QMainWindow):
+class Client(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(400, 300, 600, 500)
-        self.setWindowIcon(QIcon("pyqt5icons/sunny.jpg"))
-        label = QLabel("asdasd", self)
-        piximap = QPixmap("pyqt5icons/guiimage.jpg")
-        piximap.scaled(label.width(), label.height())
-        label.setGeometry(0, 0, 550, 450)
-        label.setPixmap(piximap)
-        label.show()
+        self.setWindowTitle("Placeholder")
+        self.setGeometry(500, 200, 800, 800)
+        self.initUI()
 
-    def initUi():
-        pass
+    def initUI(self):
+        window = QWidget()
+
+        self.line_edit = QLineEdit(self)
+        self.line_edit.setGeometry(0, 0, 150, 25)
+        self.line_edit.setPlaceholderText("Enter a new task to add: ")
+
+        self.button = QPushButton("Enter", self)
+        self.button.setGeometry(100, 0, 100, 25)
+        self.button.clicked.connect(self.on_click)
+
+        layout = QHBoxLayout()
+        window.setLayout(layout)
+
+    def on_click(self):
+        text = self.line_edit.text()
+        print(text)
 
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    client1 = Client()
+    client1.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
